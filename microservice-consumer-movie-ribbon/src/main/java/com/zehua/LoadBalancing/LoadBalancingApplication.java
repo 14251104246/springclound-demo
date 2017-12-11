89af3dd2b16ea8c7422d5beb0faf465031ce6a40
14251104246 <523786283@qq.com>
2017-12-11 16:18:54 +0800

    通过@EnableDiscoveryClient注解，将负载均衡微服务注册到Eureka上面去。并使用@LoadBalanced注解，为RestTemplate开启负载均衡的能力。

---
+++ microservice-consumer-movie-ribbon/src/main/java/com/zehua/LoadBalancing/LoadBalancingApplication.java
@@ -0,0 +1,32 @@
+package com.zehua.LoadBalancing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 负载均衡微服务
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LoadBalancingApplication {
+    /**
+     * 实例化RestTemplate，通过@LoadBalanced注解开启均衡负载能力.
+     * <p>
+     * RestTemplate可以用于调用http请求的WebService
+     *
+     * @return restTemplate
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadBalancingApplication.class, args);
+    }
+}
---
+++ microservice-consumer-movie-ribbon/src/main/java/com/zehua/LoadBalancing/controller/RibbonController.java
@@ -0,0 +1,19 @@
+package com.zehua.LoadBalancing.controller;
+
+import com.zehua.LoadBalancing.entity.User;
+import com.zehua.LoadBalancing.service.RibbonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RibbonController {
+    @Autowired
+    private RibbonService ribbonService;
+
+    @GetMapping("/ribbon/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.ribbonService.findById(id);
+    }
+}
